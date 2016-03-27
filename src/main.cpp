e29db8bfe691fa70ac708a99dca75059f54bb9a8
tastelessgreentea <tastelessgreentea@gmail.com>
2016-03-27 02:57:00 -0400

    Add sphere hit test
    Add sphere hit test function and place at Vec3(0, 0, 0.5) with a radius
    of 0.5

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include "Ray.h"
 
+bool hit_sphere(const Vec3& center, float radius, const Ray& r) {
+  Vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0 * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius*radius;
+  float discriminant = b*b - 4*a*c;
+  return (discriminant > 0);
+}
+
 Vec3 color(const Ray& r) {
+  if (hit_sphere(Vec3(0, 0, -1), 0.5, r)) {
+    return Vec3(1, 0 ,0);
+  }
+
   Vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0-t) * Vec3(1.0,1.0,1.0) + t * Vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
-  int nx = 400;
-  int ny = 200;
+  int nx = 200;
+  int ny = 100;
 
   Vec3 lower_left_corner(-2.0, -1.0, -1.0);
   Vec3 horizontal(4.0, 0.0, 0.0);
